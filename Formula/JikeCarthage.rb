39b244e8e9234fe183a09c806fe02a443230e351
mcfans <mcfans@icloud.com>
2019-02-27 11:51:48 +0800

    update formula

--- Formula/JikeCarthage.rb
+++ Formula/JikeCarthage.rb
@@ -1,8 +1,6 @@
 class Jikecarthage < Formula
     desc "Decentralized dependency manager for Cocoa"
     homepage "https://github.com/Carthage/Carthage"
-    url "https://github.com/ruguoapp/Carthage.git"
-    head "https://github.com/ruguoapp/Carthage.git", :shallow => false
     url "https://github.com/ruguoapp/homebrew-carthage.git"
     head "https://github.com/ruguoapp/homebrew-carthage.git", :shallow => false
   
@@ -11,3 +9,19 @@ class Jikecarthage < Formula
       sha256 "4c86ff31bf54d7ee8dad4e9921d7757c5fc6f4b62ec141e339bcfae667fb23da" => :mojave
       sha256 "99f35655d278ebe1dae617847f77ec5bbae4a8ebbe7c636c7912c53902c0e7a8" => :high_sierra
     end
+  
+    depends_on :xcode => ["9.4", :build]
+  
+    def install
+      system "make", "prefix_install", "PREFIX=#{prefix}"
+      bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+      zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+      fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+    end
+  
+    test do
+      (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+      system bin/"carthage", "update"
+    end
+  end
+  
