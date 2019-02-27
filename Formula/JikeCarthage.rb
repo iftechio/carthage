65f02b33beb071e964e044577b059660e9a1bc11
mcfans <mcfans@icloud.com>
2019-02-27 15:39:01 +0800

    wish this one work

--- Formula/JikeCarthage.rb
+++ Formula/JikeCarthage.rb
@@ -1,34 +1,26 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Jikecarthage < Formula
-  desc "A simple, decentralized dependency manager for Cocoa"
-  homepage ""
-  url "https://github.com/ruguoapp/homebrew-carthage/releases/download/0.32.0/carthage.tar.gz"
-  sha256 "9c3671054b7db0f6e3b95b00956666bef47dd3930f05be9e57b6208732502efe"
-  # depends_on "cmake" => :build
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  url "https://github.com/ruguoapp/homebrew-carthage.git",
+      :shallow  => false
+  head "https://github.com/ruguoapp/homebrew-carthage.git", :shallow => false
+
+  bottle do
+    cellar :any
+    sha256 "0831024b01d3de78e52c859abf43871d4ee03edbf6984cc9035b3d98547c2267" => :mojave
+  end
+
+  depends_on :xcode => ["9.4", :build]
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-carthage`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
   end
 end
