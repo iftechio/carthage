81a66b13bb0ea07fd92755553e38f94066b4e674
mcfans <mcfans@icloud.com>
2019-02-27 13:20:41 +0800

    remove bottle

--- Formula/JikeCarthage.rb
+++ Formula/JikeCarthage.rb
@@ -3,13 +3,7 @@ class Jikecarthage < Formula
     homepage "https://github.com/Carthage/Carthage"
     url ".../carthage.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     head "https://github.com/ruguoapp/homebrew-carthage.git", :shallow => false
-  
-    bottle do
-      cellar :any
-      sha256 "4c86ff31bf54d7ee8dad4e9921d7757c5fc6f4b62ec141e339bcfae667fb23da" => :mojave
-      sha256 "99f35655d278ebe1dae617847f77ec5bbae4a8ebbe7c636c7912c53902c0e7a8" => :high_sierra
-    end
-  
+    
     depends_on :xcode => ["9.4", :build]
   
     def install
